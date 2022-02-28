3c9560a5dd1544a0ce91c8d3f1ee4f12f6871a36
kevink2023 <kevink2029@gmail.com>
2022-02-28 17:27:12 -0500

    testing 1.0

--- hw6kelly/hw6kelly.c
+++ hw6kelly/hw6kelly.c
@@ -198,16 +198,16 @@ void *ThreadMotor( void * arg  )
         parameter->pwm->DAT1 = PWM;
       }
 
-    } else if (mode == MODE_2) {
+    } else if ( I1 && (mode == MODE_2)) {
       while(GPIO_READ(parameter->gpio, parameter->IR_pin) != 0) {
-        printf("\n%s MOTOR: mode 2 turn step\n", parameter->left_motor ? "LEFT" : "RIGHT");
         if(left){
-          parameter->pwm->DAT1 += PWM_MODE2_STEP;
-          parameter->pwm->DAT2 -= PWM_MODE2_STEP;
-        } else {
-          parameter->pwm->DAT2 += PWM_MODE2_STEP;
           parameter->pwm->DAT1 -= PWM_MODE2_STEP;
+          parameter->pwm->DAT2 += PWM_MODE2_STEP;
+        } else {
+          parameter->pwm->DAT2 -= PWM_MODE2_STEP;
+          parameter->pwm->DAT1 += PWM_MODE2_STEP;
         }
+        printf("\n%s MOTOR: mode 2 turn step\n", parameter->left_motor ? "LEFT" : "RIGHT");
       }
       if(left){
         parameter->pwm->DAT2 = PWM;

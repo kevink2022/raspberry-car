d4e6fb95afc56589472c15cb11a6a72f313916ce
kevink2023 <kevink2029@gmail.com>
2022-02-28 19:24:17 -0500

    testing 1.9.3

--- hw6kelly/hw6kelly.c
+++ hw6kelly/hw6kelly.c
@@ -203,12 +203,12 @@ void *ThreadMotor( void * arg  )
       if(GPIO_READ(parameter->gpio, parameter->IR_pin) != 0){
         if(left){
           //parameter->pwm->DAT1 -= PWM_MODE2_STEP;
-          parameter->pwm->DAT2 += PWM_MOTOR_MAX;
-          parameter->pwm->DAT1 += PWM_MOTOR_MIN;
+          parameter->pwm->DAT2 = PWM_MOTOR_MAX;
+          parameter->pwm->DAT1 = PWM_MOTOR_MIN;
         } else {
           //parameter->pwm->DAT2 -= PWM_MODE2_STEP;
-          parameter->pwm->DAT1 += PWM_MOTOR_MAX;
-          parameter->pwm->DAT2 += PWM_MOTOR_MIN;
+          parameter->pwm->DAT1 = PWM_MOTOR_MAX;
+          parameter->pwm->DAT2 = PWM_MOTOR_MIN;
         }
         while(GPIO_READ(parameter->gpio, parameter->IR_pin) != 0) { 
         }

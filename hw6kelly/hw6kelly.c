8473bd9dcf82bb309089431bf3ff0ef11a13434a
kevink2023 <kevink2029@gmail.com>
2022-03-01 12:15:15 -0500

    new 0.7

--- hw6kelly/hw6kelly.c
+++ hw6kelly/hw6kelly.c
@@ -210,7 +210,7 @@ void *ThreadMotor( void * arg  )
 
     if ( (AI1 && BI1) && (mode == MODE_2)) {
       if(GPIO_READ(parameter->gpio, parameter->AIR_pin) != 0){
-        while(GPIO_READ(parameter->gpio, parameter->BIR_pin) != 0) { 
+        while(GPIO_READ(parameter->gpio, parameter->BIR_pin) == 0) { 
           parameter->pwm->DAT2 = PWM_MOTOR_MAX;
           GPIO_CLR( parameter->gpio, parameter->AI1_pin );
           GPIO_CLR( parameter->gpio, parameter->AI2_pin );

f5256aae66f6e16043cc57cf6f00ac5cfe124469
kevink2023 <kevink2029@gmail.com>
2022-03-01 12:20:22 -0500

    new 0.8

--- hw6kelly/hw6kelly.c
+++ hw6kelly/hw6kelly.c
@@ -210,17 +210,17 @@ void *ThreadMotor( void * arg  )
 
     if ( (AI1 && BI1) && (mode == MODE_2)) {
       if(GPIO_READ(parameter->gpio, parameter->AIR_pin) != 0){
-        parameter->pwm->DAT2 = PWM_MOTOR_MAX;
-        GPIO_CLR( parameter->gpio, parameter->AI1_pin );
-        GPIO_CLR( parameter->gpio, parameter->AI2_pin );
-        while(GPIO_READ(parameter->gpio, parameter->BIR_pin) != 0) { 
+        while(GPIO_READ(parameter->gpio, parameter->BIR_pin) != 0) {
+          parameter->pwm->DAT2 = PWM_MOTOR_MAX;
+          GPIO_CLR( parameter->gpio, parameter->AI1_pin );
+          GPIO_CLR( parameter->gpio, parameter->AI2_pin ); 
         }
       }
       if(GPIO_READ(parameter->gpio, parameter->BIR_pin) != 0){
-        parameter->pwm->DAT1 = PWM_MOTOR_MAX;
-        GPIO_CLR( parameter->gpio, parameter->BI1_pin );
-        GPIO_CLR( parameter->gpio, parameter->BI2_pin );
-        while(GPIO_READ(parameter->gpio, parameter->AIR_pin) != 0) { 
+        while(GPIO_READ(parameter->gpio, parameter->AIR_pin) != 0) {
+          parameter->pwm->DAT1 = PWM_MOTOR_MAX;
+          GPIO_CLR( parameter->gpio, parameter->BI1_pin );
+          GPIO_CLR( parameter->gpio, parameter->BI2_pin ); 
         }
       }
       parameter->pwm->DAT2 = A_PWM;

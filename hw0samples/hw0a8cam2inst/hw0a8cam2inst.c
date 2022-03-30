72b4d257013bcda2ce778986eb38713b8692b8d4
kevink2023 <kevink2029@gmail.com>
2022-03-30 14:20:59 -0400

    testing 80x60 image 2.8

--- hw0samples/hw0a8cam2inst/hw0a8cam2inst.c
+++ hw0samples/hw0a8cam2inst/hw0a8cam2inst.c
@@ -135,7 +135,7 @@ int main( int argc, char *argv[] )
                     }
                   }
                 }
-                if(bx%10 == 0){ printf("%i", by); }
+                if(1){ printf("%i", bx); }
               }
               
             }

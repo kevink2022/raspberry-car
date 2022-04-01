8b260f62b4e39eda69da39e3917c00ef02455efb
kevink2023 <kevink2019@gmail.com>
2022-03-31 22:13:28 -0400

    slower top speed

--- hw8kelly/hw8kelly.c
+++ hw8kelly/hw8kelly.c
@@ -520,10 +520,6 @@ void *ThreadCamera( void * arg  )
       }
 
       if (diverge_point < 3){
-        turn = (PWM_MOTOR_MIN + (offsets[diverge_point]/5)*10);
-
-        if(turn > PWM_MOTOR_MAX){turn = PWM_MOTOR_MAX;}
-
         if(offsets[diverge_point] > 0){
           printf("**********LEFT**********\n offset:   %i\n", offsets[diverge_point]);
           local_pin_values.B_PWM = PWM_MOTOR_MIN + JUICE;// + 2*abs(offsets[diverge_point]);
@@ -558,8 +554,8 @@ void *ThreadCamera( void * arg  )
         for (bx = 1; bx < DIVERGE_CUTOFF; bx++){
           printf("%i | ", offsets[bx]);
         } printf("\n");
-        local_pin_values.A_PWM = PWM_MOTOR_MAX;
-        local_pin_values.B_PWM = PWM_MOTOR_MAX;
+        local_pin_values.A_PWM = PWM_MOTOR_MAX - 20;
+        local_pin_values.B_PWM = PWM_MOTOR_MAX - 20;
         local_pin_values.AI1 = 0;
         local_pin_values.AI2 = 1;
         local_pin_values.BI1 = 0;
